70b74909ad93c3893ee36e852f5fd795b8a45a77
MEMOPERF <contact@checkupauto.fr>
2026-09-12 12:22:31 +0200

    fix(orch9i): point policy tests at restored suite

--- src/policy_t_02.inc.rs
+++ src/policy_t_02.inc.rs
@@ -2,7 +2,5 @@
 mod tests {
     use super::*;
     use std::time::{SystemTime, UNIX_EPOCH};
-    include!("policy_tests_01.inc.rs");
-    include!("policy_tests_02.inc.rs");
-    include!("policy_tests_03.inc.rs");
+    include!("policy_tests.inc.rs");
 }
