e4aeaf2b174f316e4bc45423d1ba4f9e89c70322
MEMOPERF <contact@checkupauto.fr>
2026-09-12 10:44:56 +0200

    refactor(orch9i): split PolicySnapshot driver includes

--- src/policy.rs
+++ src/policy.rs
@@ -1,4 +1,5 @@
-include!("policy_a.inc.rs");
+include!("policy_a1.inc.rs");
+include!("policy_a2.inc.rs");
 include!("policy_b.inc.rs");
 include!("policy_c.inc.rs");
 include!("policy_tests.inc.rs");
